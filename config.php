df2e9af4f701a64766236e4369153837b049414d
Bertawsky031 <bertawsky02@gmail.com>
2023-11-24 22:15:00 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+date_default_timezone_set("Asia/Jakarta");
+error_reporting(0);
+
+	// sesuaikan dengan server anda
+	$host 	= 'localhost';
+	$user 	= 'root';
+	$pass 	= ''; 
+	$dbname = 'toko22';
+	
+	try{
+		$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
+		//echo 'sukses';
+	}catch(PDOException $e){
+		echo 'KONEKSI GAGAL' .$e -> getMessage();
+	}
+	
+	$view = 'fungsi/view/view.php'; // direktori fungsi select data
+?>
+
